95b68166f2ee92048c7ce01976a9d10fef15124c
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-09 06:38:17 +0700

    Create auth.php
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ lang/in/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Kredensial ini tidak ada dalam catatan kami.',
+    'password' => 'Salah password.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Coba lagi dalam :seconds detik.',
+
+];
