0aec987378297c21fac24f749b87a3f56c92bb20
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-08 22:48:15 +0700

    Create HeadSlot.vue
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/js/Components/HeadSlot.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ title: String })
+</script>
+
+ <template>
+ <Head>
+<title><slot></slot></title>
+<meta head-key="description" name="description" content="Website tentang Yayasan Dar Al Furqon Al Hakim" />
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+</Head>
+</template>
