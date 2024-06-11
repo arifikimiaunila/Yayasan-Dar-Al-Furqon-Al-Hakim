ae6df953ea4c29c63c3f9eb2341e15f46c2412cf
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-11 07:16:39 +0700

    Create presentYear.ts
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/js/ts part/presentYear.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useYear = defineStore('storeYear', {
+  state: () => ({
+  d: new Date() 
+  }),
+actions: {
+presentYear(){
+this.d.getFullYear()
+}
+}
+})
