b87dd82d4b8b518e2962c9dd3fbc6c405d0ffc17
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-11 07:33:02 +0700

    Update Footer.vue
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,13 +1,13 @@
-<script setup>
+<script setup lang='ts'>
 import {useYear} from '@/ts part/presentYear.ts'
 import { storeToRefs } from 'pinia'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-const nowYear = useYear()
- const { d } = storeToRefs(nowYear)
+const nowYear: object = useYear()
+ const { d }: object = storeToRefs(nowYear)
 // the increment action can just be destructured
-const { presentYear } = nowYear
+const { presentYear }: object = nowYear
 </script>
 <template>
 <footer class="a354-container a354-blue-ray">
