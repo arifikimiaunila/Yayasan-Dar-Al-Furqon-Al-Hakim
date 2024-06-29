38788a989afa7e378c01fc82185b6794fb8035c9
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-29 12:59:45 +0700

    Update app.php
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\PreventRequestsDuringMaintenance;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\TrimStrings;
+use App\Http\Middleware\TrustHosts;
+use App\Http\Middleware\TrustProxies;
+use App\Http\Middleware\ValidateSignature;
+use App\Http\Middleware\VerifyCsrfToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,18 +19,22 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
-  ->withMiddleware(function (Middleware $middleware) {
-$middleware->statefulApi();
-$middleware->group('web', [ \Illuminate\Cookie\Middleware\EncryptCookies::class, \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,        \Illuminate\Session\Middleware\StartSession::class,        \Illuminate\View\Middleware\ShareErrorsFromSession::class,        \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,        \Illuminate\Routing\Middleware\SubstituteBindings::class, \Illuminate\Session\Middleware\AuthenticateSession::class,
-App\Http\Middleware\TeamsPermission::class
-    ]);
-    $middleware->group('api', [
- \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        // 'throttle:api',
-     \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ]);
+    )->withMiddleware(function (Middleware $middleware) {
+	   $middleware->web(append: [
+            HandleInertiaRequests::class,
+            Authenticate::class,
+		   PreventRequestsDuringMaintenance::class,
+		   RedirectIfAuthenticated::class,
+		   TrimStrings::class,
+		   TrustProxies::class,
+		   ValidateSignature::class,
+		   VerifyCsrfToken::class,
+        ]);
 })
     ->withExceptions(function (Exceptions $exceptions) {
-     //
+    $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        });
 })->create();
