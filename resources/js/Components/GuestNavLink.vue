29deba418c39d205834286b185950d77fa1740f5
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-29 13:16:15 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/js/Components/GuestNavLink.vue
@@ -0,0 +1,62 @@
+<script setup lang='ts'>
+import { createApp } from 'vue'
+import { Link } from '@inertiajs/vue3'
+
+ interface Props {
+  yayasans: any,
+pengurusyayasans: any,
+tk: any,
+sekolahmenengah: any
+info: any
+}
+
+const props = withDefaults(defineProps<Props>(), {
+yayasans: ()=>[
+        {name: 'Sejarah Yayasan', url:( 'posts.show', 1)},
+        {name: 'AD/ART Yayasan', url: ( 'posts.show', 2)}],
+pengurusyayasans: ()=>[
+        {name: 'Pengurus Yayasan', url: ('pengurus_yayasan.index')},
+{name: 'Fasilitas Yayasan', url: ('posts.show', 3)},
+{name: 'Kontak Yayasan', url: ('datayayasan.show', 1)}
+],
+tk: ()=> [
+        {name: 'Sejarah TK', url:( 'posts.show', 4)},
+        {name: 'AD/ART TK', url: ( 'posts.show', 5)},
+        {name: 'Pengurus TK', url: 'pengurus_yayasan.index'},
+{name: 'Fasilitas TK', url: ('posts.show', 6)},
+{name: 'Kontak TK', url: ('datayayasan.show', 1)}
+],
+sekolahmenengah: ()=> [
+        {name: 'SMP', url: 'https://smpafbs.sch.id/'},
+        {name: 'SMA', url: 'https://www.smaafbs.sch.id/'}
+],
+info: ()=> [
+        {name: 'Pengumuman', url: 'fles.index'},
+        {name: 'video', url: 'video.index'}
+]
+}
+)
+</script>
+   <template>
+<nav id="guestnavlink" class="a354-bar a354-mobile a354-light-grey ">
+<Link :href="route('home')" class="a354-bar-item a354-button">Home</Link>
+<div class="a354-dropdown-click" >
+      <button class="a354-button" v-show="showDiv" v-for="x in yayasans">
+    <Link class="a354-bar-item w3-button" :href="route(x.url)">{{ x.name }}</Link>
+ </button>
+</div>
+<div class="a354-dropdown-click" >
+      <button class="a354-button" v-show="showDiv" v-for="x in tk">
+    <Link class="a354-bar-item a354-button" :href="route(x.url)">{{ x.name }}</Link>
+ </button>
+</div>
+<div v-for="x in sekolahmenengah">
+    <Link class="a354-bar-item a354-button" :href="route(x.url)">{{ x.name }}</Link>
+</div>
+<div class="a354-dropdown-click" >
+      <button class="a354-button" v-show="showDiv" v-for="x in info">
+    <Link class="a354-bar-item a354-button" :href="route(x.url)">{{ x.name }}</Link>
+ </button>
+</div>
+</nav>
+</template>
---
+++ resources/js/Components/tableslot.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+interface Props {
+  fields: string[]
+}
+
+</script>
+<template>
+  </div>
+<table class="a354-table-all a354-striped a354-hoverable a354-bordered css-serial">
+  <thead>
+    <tr class="a354-center a354-large a354-text-blue">
+      <!-- loop through each value of the fields to get the table header -->
+      <th  v-for="field in fields" :key='field' > 
+    <b>  
+  {{field}}
+</b>
+       </th>
+    </tr>
+  </thead>
+  <tbody>
+      <slot></slot>
+  </tbody>
+</table> 
+</template>
