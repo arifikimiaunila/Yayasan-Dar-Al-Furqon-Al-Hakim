20fa75e4391cb7226f408d673440e3571f3ad61e
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-29 13:14:05 +0700

    Create GuestLayout.vue
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/js/Layout/GuestLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+import GuestHeader from '@/Components/GuestHeader.vue'
+import GuestNavLink from '@/Components/GuestNavLink.vue'
+import Footer from '@/Components/Footer.vue'
+</script>
+
+<template>
+<GuestHeader/>
+<GuestNavLink/>
+<slot></slot>
+ <Footer/>
+</template>
