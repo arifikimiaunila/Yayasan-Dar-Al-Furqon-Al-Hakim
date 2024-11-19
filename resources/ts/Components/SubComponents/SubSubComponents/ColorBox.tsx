8e8fea0c9a37aef1736369a21fd7042f336d7782
arifikimiaunila <arifikimiaunila@gmail.com>
2024-11-20 02:44:00 +0700

    Update ColorBox.tsx

--- resources/ts/Components/SubComponents/SubSubComponents/ColorBox.tsx
+++ resources/ts/Components/SubComponents/SubSubComponents/ColorBox.tsx
@@ -50,10 +50,11 @@ const ColorLists= forwardRef<Ref, ColorProps>((ColorProps, ref)=>(warnas.map(war
   </Button>)));
 
 const ColorButton: React.JSX.Element = 
+<div id="textColorDropdown" class="z-10 hidden w-48 rounded bg-white p-2 shadow dark:bg-gray-700"> <div class="grid grid-cols-6 gap-2 group mb-3 items-center p-1.5 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600"> <input type="color" id="color" value="#e66465" class="border-gray-200 border bg-gray-50 dark:bg-gray-700 dark:border-gray-600 rounded-md p-px px-1 hover:bg-gray-50 group-hover:bg-gray-50 dark:group-hover:bg-gray-700 w-full h-8 col-span-3" /> <label for="color" class="text-gray-500 dark:text-gray-400 text-sm font-medium col-span-3 group-hover:text-gray-900 dark:group-hover:text-white">Pick a color</label> </div> 
 <Button.Group>
 {ColorLists}
 </Button.Group>
+</div>
 ;
 
-return ColorButton;
-}
+return ColorButton;
