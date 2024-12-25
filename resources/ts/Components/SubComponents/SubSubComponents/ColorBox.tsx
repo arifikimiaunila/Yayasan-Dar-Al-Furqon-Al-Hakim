180cdc1dca832b55711854c15fed5266b5508053
arifikimiaunila <arifikimiaunila@gmail.com>
2024-12-26 05:58:24 +0700

    Update ColorBox.tsx

--- resources/ts/Components/SubComponents/SubSubComponents/ColorBox.tsx
+++ resources/ts/Components/SubComponents/SubSubComponents/ColorBox.tsx
@@ -1,17 +1,15 @@
 import { Button} from "flowbite-react";
-import React from "react";
 import { ReactNode, forwardRef} from "react";
 
-interface ColorProps {
+interface Props {
   tipe: "button";
   dhcolor: string, 
   namacolor?: ReactNode,
   }
 
 type Ref = HTMLButtonElement;
-// eslint-disable-next-line @typescript-eslint/no-array-constructor
 
-const warnas: ColorProps[] = [
+const warnas: Props[] = [
     { dhcolor: "#1A56DB", namacolor: "Blue", tipe: "button" },
     { dhcolor: "#0E9F6E", namacolor: "Green", tipe: "button" },
     { dhcolor: "#FACA15", namacolor: "Yellow", tipe: "button" },
@@ -48,22 +46,7 @@ const warnas: ColorProps[] = [
     { dhcolor: "#F9FAFB", namacolor: "Heaven Gray", tipe: "button" },
 ];
    
-const ColorLists= forwardRef<Ref, ColorProps>((ColorProps, ref)=>(warnas.map(warna=><Button outline gradientDuoTone="purpleToBlue"
+export const ColorLists = forwardRef<Ref, Props>((_Props, ref)=>(warnas.map(warna=><Button outline gradientDuoTone="purpleToBlue"
   data-hex-color={warna.dhcolor} ref={ref} style={{ backgroundColor: warna.dhcolor }} type={warna.tipe}>
   {warna.namacolor}
   </Button>)));
-
-export const ColorButton: React.JSX.Element = 
-<>
-<div id="textColorDropdown" className="z-10 hidden w-48 rounded bg-white p-2 shadow 
-dark:bg-gray-700">
-<div className="grid grid-cols-6 gap-2 group mb-3 items-center p-1.5 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600">
-<input type="color" id="color" value="#e66465" className="border-gray-200 border bg-gray-50 dark:bg-gray-700 dark:border-gray-600 rounded-md p-px px-1 hover:bg-gray-50 group-hover:bg-gray-50 dark:group-hover:bg-gray-700 w-full h-8 col-span-3" aria-label="Pilih salah satu warna!" />
-<label htmlFor="color" className="text-gray-500 dark:text-gray-400 text-sm font-medium col-span-3 group-hover:text-gray-900 dark:group-hover:text-white">Pick a color</label>
-</div>
-<Button.Group>
-{ColorLists}
-</Button.Group>
-</div>
-</>
-;
