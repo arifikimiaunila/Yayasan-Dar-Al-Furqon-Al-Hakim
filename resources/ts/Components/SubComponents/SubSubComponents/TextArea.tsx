863f57aecf9bb9821e4db4acd261f24677d6aba3
arifikimiaunila <arifikimiaunila@gmail.com>
2024-12-26 06:02:33 +0700

    Update TextArea.tsx

--- resources/ts/Components/SubComponents/SubSubComponents/TextArea.tsx
+++ resources/ts/Components/SubComponents/SubSubComponents/TextArea.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type  TextProps = {
   tujuan: string,
   judul: string,
@@ -7,8 +5,9 @@ type  TextProps = {
 }
 
 export default function TextArea({tujuan, judul, tambahan}:TextProps){
-let x: number= tambahan.length;
-let d: String= tambahan.substring(0, x);
+const x: number= tambahan.length;
+// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+const d: String = tambahan.substring(0, x);
 return (
  <>
  <label htmlFor={tujuan} className="sr-only" {...d}>{judul}</label>
@@ -16,7 +15,7 @@ return (
    id="editor"
    rows={8}
    className="block w-full px-0 text-sm text-gray-800 bg-white border-0 
-     dark:bg-gray-800 dark:focus:ring-0 dark:text-white dark:placeholder-gray-400"
+     dark:bg-gray-800 dark:focus:ring-0 dark:text-white dark:placeholder-gray-400 overflow-scroll"
    placeholder="Write an article..."
    required
    {...d}
