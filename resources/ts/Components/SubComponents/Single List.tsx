029431b66d3b866be177cbd06fd75d2c0fcb5bd5
arifikimiaunila <arifikimiaunila@gmail.com>
2025-01-29 10:30:05 +0700

    Update Single List.tsx

--- resources/ts/Components/SubComponents/Single List.tsx
+++ resources/ts/Components/SubComponents/Single List.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@inertiajs/react';
+import React from 'react';
 
 interface SingleListProps {
  link: string;
@@ -7,6 +8,9 @@ interface SingleListProps {
 
 export default function SingleList({link, title }: SingleListProps) {
   return (
-   <li> <Link href={link} class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">{title}</Link> </li>
+<>
+   <li> <Link href={link} className="block py-2 px-3 text-white bg-blue-700 rounded 
+   md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">{title}</Link> </li>
+</>
   );
 }
