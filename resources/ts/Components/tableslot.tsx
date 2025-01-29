572a2297a597265e6615f05d0e5cf9108d8993be
arifikimiaunila <arifikimiaunila@gmail.com>
2025-01-29 10:53:21 +0700

    Update ActionSection.tsx

--- resources/ts/Components/tableslot.tsx
+++ resources/ts/Components/tableslot.tsx
@@ -1,23 +1,25 @@
-<script setup lang='ts'>
+import React, { PropsWithChildren } from 'react';
+import SectionTitle from '@/Components/SectionTitle';
+
 interface Props {
-  fields: string[]
+  title: string;
+  description: string;
 }
 
-</script>
-<template>
-<table class="a354-table-all a354-striped a354-hoverable a354-bordered css-serial">
-  <thead>
-    <tr class="a354-center a354-large a354-text-blue">
-      <!-- loop through each value of the fields to get the table header -->
-      <th  v-for="field in fields" :key='field' > 
-    <b>  
-  {{field}}
-</b>
-       </th>
-    </tr>
-  </thead>
-  <tbody>
-      <slot></slot>
-  </tbody>
-</table> 
-</template>
+export default function ActionSection({
+  title,
+  description,
+  children,
+}: PropsWithChildren<Props>) {
+  return (
+    <div className="md:grid md:grid-cols-3 md:gap-6">
+      <SectionTitle title={title} description={description} />
+
+      <div className="mt-5 md:mt-0 md:col-span-2">
+        <div className="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
