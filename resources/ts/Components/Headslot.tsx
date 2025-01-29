7acaefcb41d7a1388687bd9275316e32af7befbc
arifikimiaunila <arifikimiaunila@gmail.com>
2025-01-29 10:39:41 +0700

    Update Headslot.tsx

--- resources/ts/Components/Headslot.tsx
+++ resources/ts/Components/Headslot.tsx
@@ -1,11 +1,15 @@
 import { Head } from '@inertiajs/react';
+import React, { JSXElementConstructor } from 'react';
+import { ReactNode } from 'react';
 
-export default function mainhead(props): HTMLHeadElement {
-  let mainhead:HTMLHeadElement =<Head>
-<title>
-{ props.title }<title>
-<meta head-key="description" name="description" content="Website tentang Yayasan Dar Al Furqon Al Hakim" />
-<link rel="icon" type="image/x-icon" href="favicon.ico">
-</Head>;
-return mainhead;
-}
+export function mainhead(title: string): ReactNode{
+  return 
+  <>
+  <Head>
+    <title>{title}</title>
+    <meta head-key="description" name="description" content="Website tentang Yayasan Dar Al Furqon Al Hakim" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+  </Head>
+  </>
+};
+
