99f073a8c9eef7ca2441c8bbeed078f9b1175782
arifikimiaunila <arifikimiaunila@gmail.com>
2025-01-29 10:28:18 +0700

    Update Drop Faces.tsx

--- resources/ts/Components/SubComponents/Drop Faces.tsx
+++ resources/ts/Components/SubComponents/Drop Faces.tsx
@@ -1,26 +1,38 @@
 import { Link } from '@inertiajs/react';
+import { ReactNode } from 'react';
+import React from 'react';
+import { MegaMenu } from 'flowbite-react';
+import { useRoute } from 'ziggy-js';
+import { Ziggy } from '@/ziggy.js';
 
-interface mydropface{
+const route = useRoute(Ziggy);
+
+export type mydropface={
   id: number;
   link: string;
   title: string;
 }
 
-export default function dropface(mtitle: string, himpunan: string[]){
-const list: mydropface;
- 
-const listItems: string[]=himpunan.map(list=>
-<li key={list.id}> 
-<Link href={route(list.link.valueOf())} classaName="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{list.title}</Link> 
+export default function Dropface(himpunan: mydropface[]): ReactNode{
+const route = useRoute(Ziggy);
+const listItems: ReactNode=himpunan.map(list=>
+<li key={list.id}>
+<Link href={route(list.link.valueOf())}
+className="hover:text-primary-600 dark:hover:text-primary-500">{list.title}</Link>
 </li>);
 
 return(
-<li> <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">{mtitle} <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/> </svg></button>
-<div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
+<>
+<MegaMenu.Dropdown>
+<div className="mx-auto mt-6 grid max-w-screen-xl
+border-y border-gray-200 px-4 py-5 text-sm
+text-gray-500 dark:text-gray-400 md:grid-cols-3 md:px-6">
+<ul className="space-y-4
+sm:mb-4 md:mb-0">
 {listItems}
 </ul>
 </div>
-</li>
+</MegaMenu.Dropdown>
+</>
 );
 }
