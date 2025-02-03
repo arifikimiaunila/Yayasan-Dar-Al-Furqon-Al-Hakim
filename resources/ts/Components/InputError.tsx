95135ad68c49fe1e024693784dc3cf51210e1ae0
arifikimiaunila <arifikimiaunila@gmail.com>
2025-02-03 13:47:00 +0700

    Update InputError.tsx

--- resources/ts/Components/InputError.tsx
+++ resources/ts/Components/InputError.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 interface Props {
-  title: string;
-  description: string;
+  message?: string;
+  className?: string;
 }
 
-export default function SectionTitle({ title, description }: Props) {
+export default function InputError({
+  message,
+  className,
+  children,
+}: PropsWithChildren<Props>) {
+  if (!message && !children) {
+    return null;
+  }
   return (
-    <div className=
-    "md:col-span-1">
-      <div className="px-4
-      sm:px-0">
-        <h3 className="text-lg font-medium text-gray-900
-        dark:text-gray-100">
-          {title}
-        </h3>
-
-        <p className="mt-1 text-sm text-gray-600
-        dark:text-gray-400">
-          {description}
-        </p>
-      </div>
+    <div className={className}>
+      <p className="text-sm text-red-600 dark:text-red-400">
+        {message || children}
+      </p>
     </div>
   );
 }
