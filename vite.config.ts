b6ef7511354683d6c5b4f1d0aa5f40061e2c8523
arifikimiaunila <arifikimiaunila@gmail.com>
2025-02-12 14:44:51 +0700

    Update vite.config.ts
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@ import laravel from 'laravel-vite-plugin';
 import commonjs from '@rollup/plugin-commonjs';
 import manifestSRI from 'vite-plugin-manifest-sri';
 import ziggy from 'vite-plugin-ziggy';
-import * as path from 'path';
 import  tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/ts/app.jsx'],
-      ssr: 'resources/ts/ssr.jsx',
+      input: ['resources/css/app.css', 'resources/stories/ts/app.jsx'],
+      ssr: 'resources/stories/ts/ssr.jsx',
       refresh: [
         {
           paths: ['path/to/watch/**'],
@@ -29,11 +28,6 @@ export default defineConfig({
   // Add your other plugins here, e.g. vue(), vueI18n(),...
   ziggy()
 ],
-resolve: {
-  alias: {
-  '@': path.resolve('/resources/ts')
-  }
-},
 ssr: {
   noExternal: ['@inertiajs/server'],
 },
