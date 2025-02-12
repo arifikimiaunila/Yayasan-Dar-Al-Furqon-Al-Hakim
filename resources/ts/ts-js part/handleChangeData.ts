0e9914d29f507319c74fb81cab48c24ec2f9b1ba
arifikimiaunila <arifikimiaunila@gmail.com>
2025-02-12 08:23:12 +0700

    Delete resources directory
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- resources/ts/ts-js part/handleChangeData.ts
+++
@@ -1,9 +0,0 @@
-export function handleChange(e: React.FormEvent<HTMLInputElement>) {
-    const key: string = (e.target as HTMLInputElement).id;
-    const value: string = (e.target as HTMLInputElement).value;
-    setValues((values: any) => ({
-        ...values,
-        [key]: value,
-    }))
-}
-
