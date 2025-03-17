29737db56a3e8630d930aee72f19c7aea58a65f4
arifikimiaunila <arifikimiaunila@gmail.com>
2025-03-17 12:20:46 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/ts/asssets/Box.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+interface BoxProps extends React.SVGProps<SVGSVGElement> {}
+
+const Box: React.FC<BoxProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="icon icon-tabler icons-tabler-outline icon-tabler-rectangle"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+  </svg>
+);
+export default Box;
