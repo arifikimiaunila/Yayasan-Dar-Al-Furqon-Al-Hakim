044d99c6de11028ba2153d4423f8a6e8544b59fe
arifikimiaunila <arifikimiaunila@gmail.com>
2026-05-13 02:51:09 +0700

    Create App.vue
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/ts/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="flex flex-col min-h-screen">
+    <!-- Menggunakan komponen yang telah diimpor -->
+    <Header />
+    
+    <main class="flex-1">
+      <Body />
+    </main>
+    
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Mengimpor komponen dengan TypeScript
+import Header from './Components/Header.vue';
+import Body from './Components/Body.vue';
+import Footer from './Components/Footer.vue';
+</script>
