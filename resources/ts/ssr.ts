6dfe2d9c22c2a3f6e8fa2640163c06205b97dd75
arifikimiaunila <arifikimiaunila@gmail.com>
2026-05-13 02:16:13 +0700

    Add SSR setup for Inertia.js with Vue 3
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/ts/ssr.ts
@@ -0,0 +1,20 @@
+import { createInertiaApp } from '@inertiajs/vue3'
+import createServer from '@inertiajs/vue3/server'
+import { createSSRApp, h } from 'vue'
+import { renderToString } from 'vue/server-renderer'
+
+createServer(page =>
+    createInertiaApp({
+        page,
+        render: renderToString,
+        resolve: name => {
+            const pages = import.meta.glob('./Pages/**/*.vue')
+            return pages[`./Pages/${name}.vue`]()
+        },
+        setup({ App, props, plugin }) {
+            return createSSRApp({
+                render: () => h(App, props),
+            }).use(plugin)
+        },
+    }),
+)
