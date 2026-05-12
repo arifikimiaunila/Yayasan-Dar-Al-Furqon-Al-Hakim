81b74687e4540cda821dc8bff7c440a918ecf81c
arifikimiaunila <arifikimiaunila@gmail.com>
2026-05-13 00:53:23 +0700

    Create vite.config.js
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+        vue(),
+        inertia()
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
