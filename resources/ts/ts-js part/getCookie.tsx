cbacd1efe6c82e40bdd326eedba3180c97d7ecf5
arifikimiaunila <arifikimiaunila@gmail.com>
2026-05-13 02:22:01 +0700

    Delete resources/ts/ts-js part/getCookie.tsx
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- resources/ts/ts-js part/getCookie.tsx
+++
@@ -1,8 +0,0 @@
-export function getCookie(name: any): string|undefined {
-  let value: string ='; ${document.cookie}';
-  let parts: string[] = value.split('; ${name}=');
-  let part: string|undefined = parts.pop();
-  if (parts.length === 2 && typeof part !== 'undefined'){
-    return part.split(';').shift();
-}
-}
