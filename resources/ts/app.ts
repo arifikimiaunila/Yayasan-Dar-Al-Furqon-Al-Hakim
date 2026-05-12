2fb27677263dd08df1dad2331854397873163b30
arifikimiaunila <arifikimiaunila@gmail.com>
2026-05-13 04:01:12 +0700

    Update app.ts
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,27 +1,25 @@
 import './bootstrap';
-import '../css/app.css'; // Pastikan path CSS sudah benar
+import '../css/app.css';
 
-import { DefineComponent } from 'vue';
-import { createApp, h } from 'vue';
+import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Yayasan Dar Al Furqon Al Hakim';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  id: 'app', // Biasanya defaultnya 'app', pastikan sesuai dengan @inertia di Blade kamu
-  title: (title) => `${title} - ${appName}`,
-  resolve: (name) =>
-    resolvePageComponent(
-      `./Pages/${name}.vue`, // Ubah ekstensi menjadi .vue
-      import.meta.glob<DefineComponent>('./Pages/**/*.vue') // Gunakan glob standar Vue
-    ),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el);
-  },
-  progress: {
-    color: '#4B5563',
-  },
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => 
+        resolvePageComponent(
+            `./Pages/${name}.vue`, 
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
