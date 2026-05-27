3d6459ad67a138cd133e4f920cabdd82bd9a7aff
arifikimiaunila <arifikimiaunila@gmail.com>
2026-05-27 20:34:50 +0700

    Update handleChangeData.ts
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- resources/ts/ts-js part/handleChangeData.ts
+++ resources/ts/ts-js part/handleChangeData.ts
@@ -1,9 +1,22 @@
-export function handleChange(e: React.FormEvent<HTMLInputElement>) {
-    const key: string = (e.target as HTMLInputElement).id;
-    const value: string = (e.target as HTMLInputElement).value;
-    setValues((values: any) => ({
-        ...values,
-        [key]: value,
-    }))
+import { ref } from 'vue';
+
+// Definisikan tipe data state Anda (opsional, tapi bagus untuk TS)
+interface FormValues {
+    [key: string]: any;
 }
 
+// Inisialisasi state di Vue 3 menggunakan ref
+const values = ref<FormValues>({});
+
+export function handleChange(e: Event) {
+    // Di Vue/TS, kita cast e.target sebagai HTMLInputElement
+    const target = e.target as HTMLInputElement;
+    const key: string = target.id;
+    const value: string = target.value;
+
+    // Cara memperbarui reactive state di Vue 3
+    values.value = {
+        ...values.value,
+        [key]: value
+    };
+}
