caac00246bf9ba343f7437eaf6e383ec3bd97979
arifikimiaunila <arifikimiaunila@gmail.com>
2026-06-28 16:47:52 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/ts/app.tsx'],
+            ssr: 'resources/ts/ssr.tsx',
             refresh: true,
         }),
         tailwindcss(),
-        vue(),
-        inertia()
+        react(),
     ],
     server: {
         watch: {
