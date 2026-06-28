ea40196dff41e0b4eb683e57d1182a22c949908a
arifikimiaunila <arifikimiaunila@gmail.com>
2026-06-28 16:24:14 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\HandleInertiaRequests;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )->withMiddleware(function (Middleware $middleware) {
+       $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
+})
+    ->withExceptions(function (Exceptions $exceptions) {
+    $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        });
+})->create();
