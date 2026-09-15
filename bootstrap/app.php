4c2e19b7f0c29ca8cdcd05bc4c5890e62faa5515
arifikimiaunila <arifikimiaunila@gmail.com>
2026-09-15 09:06:09 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\HandleInertiaRequests;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -12,9 +13,25 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->group(base_path('routes/auth.php'));
+        },
+    )->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
     )->withMiddleware(function (Middleware $middleware) {
        $middleware->web(append: [
             HandleInertiaRequests::class,
+        ]);
+        $middleware->alias([
+            'role' => CheckRoleOrPermission::class,
+            'permission' => CheckRoleOrPermission::class,
+            'ability' => CheckRoleOrPermission::class,
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            'sanctum/token',
         ]);
 })
     ->withExceptions(function (Exceptions $exceptions) {
