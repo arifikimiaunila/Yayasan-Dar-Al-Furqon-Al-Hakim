e5967e0ec56c376eaa7014eb9cc4f4bfc80a054b
arifikimiaunila <arifikimiaunila@gmail.com>
2026-09-15 08:33:54 +0700

    Update web.php
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\FlesController;
 use App\Http\Controllers\VideoController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -13,37 +14,63 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', fn () => inertia('Dashboard'))->name('dashboard');
-});
-
 Route::prefix('video')->group(function () {
-    Route::get('/index', [VideoController::class, 'index'])->name('video.index');
-    Route::get('/create', [VideoController::class, 'create'])->name('video.create');
-    Route::post('/store', [VideoController::class, 'store'])->name('video.store');
-    Route::get('/edit', [VideoController::class, 'edit'])->name('video.edit');
-    Route::get('/{no_video}/choose', [VideoController::class, 'choose_one'])->name('video.choose');
-    Route::get('/{no_video}', [VideoController::class, 'show'])->name('video.show');
-    Route::put('/{no_video}/update', [VideoController::class, 'update'])->name('video.update');
-    Route::delete('/{no_video}/delete', [VideoController::class, 'destroy'])->name('video.delete');
+    Route::view('/index', [VideoController::class, 'index'])->name('video.index');
+    Route::view('/{no_video}', [VideoController::class, 'show'])->name('video.show');
+    Route::middleware(['auth', 'role:admin2|superadmin'])->group(function () {
+        Route::view('/create', [VideoController::class, 'create'])->name('video.create');
+        Route::post('/store', [VideoController::class, 'store'])->name('video.store');
+        Route::view('/edit', [VideoController::class, 'edit'])->name('video.edit');
+        Route::view('/{no_video}/choose', [VideoController::class, 'choose_one'])->name('video.choose');
+        Route::put('/{no_video}/update', [VideoController::class, 'update'])->name('video.update');
+        Route::delete('/{no_video}/delete', [VideoController::class, 'destroy'])->name('video.delete');
+    });
 });
 
 Route::prefix('post')->group(function () {
-    Route::get('/create', [PostsController::class, 'create'])->name('post.create');
-    Route::post('/store', [PostsController::class, 'store'])->name('post.store');
-    Route::get('/edit', [PostsController::class, 'edit'])->name('post.edit');
-    Route::get('/{post_id}/choose', [PostsController::class, 'choose_one'])->name('post.choose');
-    Route::get('/{post_id}', [PostsController::class, 'show'])->name('post.show');
-    Route::put('/{post_id}/update', [PostsController::class, 'update'])->name('post.update');
-    Route::delete('/{post_id}/delete', [PostsController::class, 'destroy'])->name('post.destroy');
+    Route::view('/{post_id}', [PostsController::class, 'show'])->name('post.show');
+    Route::middleware(['auth', 'role:admin1|superadmin'])->group(function () {
+        Route::view('/create', [PostsController::class, 'create'])->name('post.create');
+        Route::post('/store', [PostsController::class, 'store'])->name('post.store');
+        Route::view('/edit', [PostsController::class, 'edit'])->name('post.edit');
+        Route::put('/{post_id}/update', [PostsController::class, 'update'])->name('post.update');
+    });
 });
 
-Route::get('/data_yayasan/{yayasan_id}/show', function (int $yayasan_id) {
-    return inertia('Beranda', ['yayasan_id' => $yayasan_id]);
-})->name('datayayasan.show');
+Route::prefix('data_yayasan')->group(function () {
+    Route::view('/{yayasan_id}', [DataYayasanController::class, 'show'])->name('data_yayasan.show', 1);
+    Route::view('/{yayasan_id}', [DataYayasanController::class, 'showcookies'])->name('data_yayasan.showcookies', 1);
+    Route::middleware(['auth', 'role:superadmin'])->group(function () {
+        Route::view('/create', [DataYayasanController::class, 'create'])->name('data_yayasan.create');
+        Route::post('/store', [DataYayasanController::class, 'store'])->name('data_yayasan.store');
+        Route::view('/edit', [DataYayasanController::class, 'edit'])->name('data_yayasan.edit');
+        Route::put('/{yayasan_id}/update', [DataYayasanController::class, 'update'])->name('data_yayasan.update');
+});
+});
 
-Route::redirect('/posts', '/post/edit')->name('posts.index');
+Route::prefix('files')->group(function () {
+    Route::view('/index', [FlesController::class, 'index'])->name('files.index');
+    Route::view('/{file_id}', [FlesController::class, 'show'])->name('files.show');
+    Route::get('/{file_id}/download', [FlesController::class, 'download'])->name('files.download');
+    Route::middleware(['auth', 'role:admin1|superadmin'])->group(function () {
+        Route::view('/create', [FlesController::class, 'create'])->name('files.create');
+        Route::post('/store', [FlesController::class, 'store'])->name('files.store');
+        Route::view('/edit', [FlesController::class, 'edit'])->name('files.edit');
+        Route::view('/{file_id}/choose', [FlesController::class, 'choose_one'])->name('files.choose');
+        Route::put('/{file_id}/update', [FlesController::class, 'update'])->name('files.update');
+        Route::delete('/{file_id}/delete', [FlesController::class, 'destroy'])->name('files.destroy');
+    });
+});
+
+Route::prefix('pengurus_yayasan')->group(function () {
+    Route::view('/index', [PengurusYayasanController::class, 'index'])->name('pengurus_yayasan.index');
+    Route::view('/{id_pengurus}', [PengurusYayasanController::class, 'show'])->name('pengurus_yayasan.show');
+    Route::middleware(['auth', 'role:admin1|superadmin'])->group(function () {
+        Route::view('/create', [PengurusYayasanController::class, 'create'])->name('pengurus_yayasan.create');
+        Route::post('/store', [PengurusYayasanController::class, 'store'])->name('pengurus_yayasan.store');
+        Route::view('/{id_pengurus}/edit', [PengurusYayasanController::class, 'edit'])->name('pengurus_yayasan.edit');
+        Route::view('/{id_pengurus}/choose', [PengurusYayasanController::class, 'choose_one'])->name('pengurus_yayasan.choose');
+        Route::put('/{id_pengurus}/update', [PengurusYayasanController::class, 'update'])->name('pengurus_yayasan.update');
+        Route::delete('/{id_pengurus}/delete', [PengurusYayasanController::class, 'destroy'])->name('pengurus_yayasan.destroy');
+    });
+});
