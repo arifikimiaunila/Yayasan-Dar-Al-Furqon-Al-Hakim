0a140dd6f188a0799a2d29c55a3d88bfcaf4508b
arifikimiaunila <arifikimiaunila@gmail.com>
2026-09-17 14:57:38 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ app/Mail/TwoFactorCodeMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TwoFactorCodeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $code;
+
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
+
+    public function build()
+    {
+        return $this->subject('Kode Verifikasi Dua Langkah')
+                    ->view('emails.twofactor')
+                    ->with(['code' => $this->code]);
+    }
+}
