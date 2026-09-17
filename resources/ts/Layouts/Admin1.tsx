8e6cfe9aefa62fc27e5d1cc62ef15bacd8d31d43
arifikimiaunila <arifikimiaunila@gmail.com>
2026-09-17 15:47:53 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/ts/Layouts/Admin1.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import { NavbarAdmin1 } from '@/Components/NavbarAdmin1';
+
+interface AppLayoutProps {
+  children?: ReactNode;
+  currentRoute?: string;
+}
+
+export default function App2({ children, currentRoute = 'home' }: AppLayoutProps) {
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="transition-opacity duration-300 ease-in-out">
+        <div className="flex min-h-screen flex-col">
+          <Header currentRoute={currentRoute} />
+          <NavbarAdmin1 />
+          <main
+            className="flex-1 bg-cover bg-center"
+            style={{ backgroundImage: "url('/path/to/your/background.jpg')" }}
+          >
+            {/* Konten utama */}
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
