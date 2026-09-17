343291e0671e435d94e94980c7365531e96478fe
arifikimiaunila <arifikimiaunila@gmail.com>
2026-09-17 15:32:18 +0700

    Create twofactor.blade.php
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

---
+++ resources/views/email/twofactor.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Kode Verifikasi Dua Langkah</title>
+</head>
+<body>
+    <h2>Kode Verifikasi Dua Langkah</h2>
+    <p>Halo,</p>
+    <p>Berikut adalah kode verifikasi dua langkah Anda:</p>
+
+    <p style="font-size: 24px; font-weight: bold; color: #2c3e50;">
+        {{ $code }}
+    </p>
+
+    <p>Kode ini berlaku selama 10 menit. Masukkan kode tersebut pada halaman verifikasi untuk melanjutkan proses login.</p>
+
+    <p>Jika Anda tidak meminta kode ini, abaikan email ini.</p>
+
+    <p>Terima kasih,<br>
+    Tim Keamanan Aplikasi</p>
+</body>
+</html>
