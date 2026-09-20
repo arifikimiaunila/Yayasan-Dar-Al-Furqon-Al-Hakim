f7629da073e06a7f765a50c36dc4f16719f26b1d
arifikimiaunila <arifikimiaunila@gmail.com>
2026-09-20 22:25:02 +0700

    Add files via upload
    Signed-off-by: arifikimiaunila <arifikimiaunila@gmail.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
- 		@routes(nonce: Vite::cspNonce())
+        @routes(nonce: Vite::cspNonce())
         @vite(['resources/css/app.css', 'resources/ts/app.tsx'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
+        @php
+            $role = request()->cookie('user_role');
+            $userId = request()->cookie('user_id');
+        @endphp
+
+        @if($role === 'admin1')
+            <script>
+                window.location.href = "{{ route('post.create') }}";
+            </script>
+        @elseif($role === 'admin2')
+            <script>
+                window.location.href = "{{ route('video.create') }}";
+            </script>
+        @elseif($role === 'superadmin')
+            <script>
+                window.location.href = "{{ route('data_yayasan.create') }}";
+            </script>
+        @endif
+
         @inertia
     </body>
 </html>
